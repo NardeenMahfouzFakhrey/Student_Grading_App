a98475966bb753ab06643687c539e3d99c978ee0
MariamRaouf30 <mariam.shenouda303@gmail.com>
2023-03-24 00:47:37 +0200

    Test cases

--- Project_Testing/src/StudentsInfoTest.java
+++ Project_Testing/src/StudentsInfoTest.java
@@ -23,6 +23,19 @@ class StudentsInfoTest {
         assertEquals(9.0, student.getPractical());
         assertEquals(16.0, student.getMidterm());
         assertEquals(45.0, student.getFinal());
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo(" John Doe", "1234567p", 8.5, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("123", "1234567p", 8.5, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "123456xp", 8.5, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "123453p", 8.5, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234537p", -10, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234547p", 12, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234517p", 8.5, 20, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234267p", 8.5, -1, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234367p", 8.5, 9.0, 25.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234767p", 8.5, 9.0, -10, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234067p", 8.5, 9.0, 16.0, 100.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo("John Doe", "1234267p", 8.5, 9.0, 16.0, 45.0));
+        Assertions.assertThrows(IllegalArgumentException.class, ()-> new StudentsInfo(" John 1Doe", "1224567p", -10, 20.0, -1, 100));
     }
 
     @Test
@@ -52,5 +65,42 @@ class StudentsInfoTest {
         assertThrows(IllegalArgumentException.class, () -> student.setID(" 123s5678"));
         assertThrows(IllegalArgumentException.class, () -> student.setID("1234s678"));
         assertThrows(IllegalArgumentException.class, () -> student.setID("123456789"));
+    }
+    @Test
+    void ActivitiesTest(){
+        student.setActivities(10);
+        assertEquals(10, student.getActivities());
+        student.setActivities(0);
+        assertEquals(0, student.getActivities());
+        student.setActivities(5.5);
+        assertEquals(5.5, student.getActivities());
+        student.setActivities(9.99);
+        assertEquals(9.99, student.getActivities());
+        assertThrows(IllegalArgumentException.class, () -> student.setActivities(100));
+        assertThrows(IllegalArgumentException.class, () -> student.setActivities(-10));
+    }
+    @Test
+    void MidtermTest(){
+        student.setMidterm(20.0);
+        assertEquals(20.0, student.getMidterm());
+        student.setMidterm(0);
+        assertEquals(0, student.getMidterm());
+        student.setMidterm(3.8);
+        assertEquals(3.8, student.getMidterm());
+        student.setMidterm(17.5);
+        assertEquals(17.5, student.getMidterm());
+        assertThrows(IllegalArgumentException.class, () -> student.setMidterm(100));
+        assertThrows(IllegalArgumentException.class, () -> student.setMidterm(-1));
+    }
+    @Test
+    void FinalTest(){
+        student.setFinal(60.0);
+        assertEquals(60.0, student.getFinal());
+        student.setFinal(35.5);
+        assertEquals(35.5, student.getFinal());
+        student.setFinal(0);
+        assertEquals(0, student.getFinal());
+        assertThrows(IllegalArgumentException.class, () -> student.setFinal(-20));
+        assertThrows(IllegalArgumentException.class, () -> student.setFinal(61));
     }
 }
